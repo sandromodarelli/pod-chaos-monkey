c4c474420f1bdadcea350297777a05cb489f3cb3
Sandro Modarelli <sandro.modarelli@namecheap.com>
2022-08-19 09:18:14 +0200

    code improvement after linting

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,4 +1,6 @@
-import pytest
+"""
+Unit tests for main module
+"""
 from unittest.mock import MagicMock, patch
 
 from ..main import list_pods, delete_random_pod
@@ -7,6 +9,12 @@ from ..main import list_pods, delete_random_pod
 @patch("config.NAMESPACE")
 @patch("kubernetes.client.api.core_v1_api.CoreV1Api")
 def test_list_pods(api, namespace):
+    """
+    Unit test for list_pods function
+    :param api: mocked API version client
+    :param namespace: mocked namespace
+    :return:
+    """
     pod1 = MagicMock()
     pod1.metadata.name = "my-app-1"
     pod1.metadata.namespace = namespace
@@ -32,6 +40,13 @@ def test_list_pods(api, namespace):
 @patch("config.NAMESPACE")
 @patch("kubernetes.client.api.core_v1_api.CoreV1Api")
 def test_delete_random_pod(api, namespace, choice_mock):
+    """
+    Unit test for delete_random_pod function
+    :param api: mocked API version client
+    :param namespace: mocked namespace
+    :param choice_mock: mocked choice to simulate random.choice
+    :return:
+    """
     pod1 = MagicMock()
     pod1.metadata.name = "my-app-1"
     pod1.metadata.namespace = namespace
@@ -48,7 +63,7 @@ def test_delete_random_pod(api, namespace, choice_mock):
 
     choice_mock.return_value = pod2
 
-    deleted_pod = delete_random_pod(api, items)
+    _ = delete_random_pod(api, items)
 
     assert api.delete_namespaced_pod.call_count == 1
     api.delete_namespaced_pod.assert_called_with(
